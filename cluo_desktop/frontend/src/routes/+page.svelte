898b1d3532ccde3283771afe273ec9409e039e9d
GaryHY <henry.gary@hotmail.com>
2025-03-15 21:37:07 +0100

    feat: add shadcn-svelte
    DO NOT REINVENT THE BUTTON

--- cluo_desktop/frontend/src/routes/+page.svelte
+++ cluo_desktop/frontend/src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script>
+    import { Button } from "$lib/components/ui/button";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
     Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
     documentation
 </p>
+<p>The first button of the app</p>
+<Button>Click me</Button>
