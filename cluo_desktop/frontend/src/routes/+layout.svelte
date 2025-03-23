69d06ad930cf41b366ef92e49f3f21dfadd36af9
GaryHY <henry.gary@hotmail.com>
2025-03-23 08:55:12 +0100

    feat: move Header and Sidebar to root layout.
    It makes more sense, since only the content has to change.

--- cluo_desktop/frontend/src/routes/+layout.svelte
+++ cluo_desktop/frontend/src/routes/+layout.svelte
@@ -2,6 +2,26 @@
     import "../app.css";
     import "../reset.css";
     let { children } = $props();
+
+    import Header from "./Header.svelte";
+    import Sidebar from "./Sidebar.svelte";
 </script>
 
-{@render children()}
+<div class="page">
+    <Header />
+    <Sidebar />
+    {@render children()}
+</div>
+
+<style lang="postcss">
+    .page {
+        display: grid;
+        grid-template-areas:
+            "header header"
+            "sidebar content";
+        /* grid-template-rows: 100px 1fr; */
+        grid-template-rows: auto 1fr;
+        grid-template-columns: auto 1fr;
+        height: 100vh;
+    }
+</style>
--- cluo_desktop/frontend/src/routes/+page.svelte
+++ cluo_desktop/frontend/src/routes/+page.svelte
@@ -1,24 +1,5 @@
 <script>
-    import Header from "./Header.svelte";
     import Content from "./Content.svelte";
-    import Sidebar from "./Sidebar.svelte";
 </script>
 
-<div class="page">
-    <Header />
-    <Sidebar />
-    <Content />
-</div>
-
-<style lang="postcss">
-    .page {
-        display: grid;
-        grid-template-areas:
-            "header header"
-            "sidebar content";
-        /* grid-template-rows: 100px 1fr; */
-        grid-template-rows: auto 1fr;
-        grid-template-columns: auto 1fr;
-        height: 100vh;
-    }
-</style>
+<Content />
