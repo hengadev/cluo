1b78141fdedcbf49136cbd01f00ffc82f98da632
GaryHY <henry.gary@hotmail.com>
2025-03-25 09:45:10 +0100

    feat: add corner buttons for header with respective tooltips

--- cluo_desktop/frontend/src/routes/Header.svelte
+++ cluo_desktop/frontend/src/routes/Header.svelte
@@ -4,6 +4,7 @@
     // import { Button } from "$lib/components/ui/button";
     import NewCase from "$lib/custom/NewCase.svelte";
     // TODO: pour la partie client ou type d'enquete, il faut un select avec tous les anciens clients + un bouton plus pour ajouter un nouvel element
+    import { Button, Tooltip } from "bits-ui";
 </script>
 
 {#snippet headerOption(Icon: any, clickAction: any)}
@@ -32,19 +33,38 @@
     </div>
 </div>
 
+{#snippet buttons(item: HeaderItem)}
+    {@const Icon = item.icon}
+    <Tooltip.Provider>
+        <Tooltip.Root delayDuration={100}>
+            <Tooltip.Trigger
+                class="border-border-input border-1 rounded-10px p-2 bg-background-alt ring-offset-background active:scale-[0.98] active:transition:all 
+		focus-visible:ring-dark focus-visible:ring-offset-background focus-visible:outline-hidden inline-flex size-16 items-center justify-center focus-visible:ring-2 focus-visible:ring-offset-2 
+                    hover:bg-[#f0f0f0]'} {item.bg} text-{item.fg}"
+            >
+                <Button.Root onclick={item.fn} class="cursor-pointer">
+                    <Icon size={32} />
+                </Button.Root>
+            </Tooltip.Trigger>
+            <Tooltip.Content sideOffset={8} side="bottom">
+                <div
+                    class="rounded-input text-[1rem] align-center bg-dark text-white font-medium border-dark-10 shadow-popover outline-hidden z-0 flex items-center justify-center border p-2"
+                >
+                    {item.title}
+                </div>
+            </Tooltip.Content>
+        </Tooltip.Root>
+    </Tooltip.Provider>
+{/snippet}
+
 <style>
     .header {
         grid-area: header;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 1rem 2rem;
+        padding: 0.5rem 2rem;
         gap: 2rem;
-    }
-    .search {
-        width: 100%;
-        max-width: 800px;
-        flex: 0 0 auto;
     }
     .left,
     .buttons {
