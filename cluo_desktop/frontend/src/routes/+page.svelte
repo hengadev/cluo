c1717410e610418bf9e26eb9258a56f857a2f78e
GaryHY <henry.gary@hotmail.com>
2025-03-26 18:19:13 +0100

    feat: move Content components to $lib/custom/content

--- cluo_desktop/frontend/src/routes/+page.svelte
+++ cluo_desktop/frontend/src/routes/+page.svelte
@@ -5,18 +5,12 @@
     import Header from "./Header.svelte";
     import Sidebar from "./Sidebar.svelte";
     import Toaster from "$lib/custom/global/toast/Toaster.svelte";
-    import Informations from "./Informations.svelte";
-    import Photos from "./Photos.svelte";
-    import Facture from "./Facture.svelte";
-    import Rapport from "./Rapport.svelte";
-    import Mandat from "./Mandat.svelte";
-    import Devis from "./Devis.svelte";
-    import Reseaux from "./Reseaux.svelte";
 
     import { setToastContext } from "$lib/custom/global/toast/state.svelte";
     setToastContext();
 
     import { type SidebarState, SIDEBAR_STATES } from "$lib/types/sidebar";
+    import Content from "./Content.svelte";
     let sidebarState = $state<SidebarState>(SIDEBAR_STATES.Informations);
 </script>
 
@@ -25,26 +19,8 @@
 <div class="page">
     <Header />
     <Sidebar bind:sidebarState />
-    {@render renderContent()}
+    <Content {sidebarState} />
 </div>
-
-{#snippet renderContent()}
-    {#if sidebarState === SIDEBAR_STATES.Informations}
-        <Informations />
-    {:else if sidebarState === SIDEBAR_STATES.Photos}
-        <Photos />
-    {:else if sidebarState === SIDEBAR_STATES.Facture}
-        <Facture />
-    {:else if sidebarState === SIDEBAR_STATES.Rapport}
-        <Rapport />
-    {:else if sidebarState === SIDEBAR_STATES.Mandat}
-        <Mandat />
-    {:else if sidebarState === SIDEBAR_STATES.Devis}
-        <Devis />
-    {:else if sidebarState === SIDEBAR_STATES.Reseaux}
-        <Reseaux />
-    {/if}
-{/snippet}
 
 <style lang="postcss">
     .page {
---
+++ cluo_desktop/frontend/src/routes/Content.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import Informations from "$lib/custom/content/informations/Informations.svelte";
+    import Photos from "$lib/custom/content/photos/Photos.svelte";
+    import Facture from "$lib/custom/content/Facture.svelte";
+    import Rapport from "$lib/custom/content/Rapport.svelte";
+    import Mandat from "$lib/custom/content/Mandat.svelte";
+    import Devis from "$lib/custom/content/Devis.svelte";
+    import Reseaux from "$lib/custom/content/Reseaux.svelte";
+
+    import { type SidebarState, SIDEBAR_STATES } from "$lib/types/sidebar";
+
+    type Props = {
+        sidebarState: SidebarState;
+    };
+    let { sidebarState }: Props = $props();
+</script>
+
+{#if sidebarState === SIDEBAR_STATES.Informations}
+    <Informations />
+{:else if sidebarState === SIDEBAR_STATES.Photos}
+    <Photos />
+{:else if sidebarState === SIDEBAR_STATES.Facture}
+    <Facture />
+{:else if sidebarState === SIDEBAR_STATES.Rapport}
+    <Rapport />
+{:else if sidebarState === SIDEBAR_STATES.Mandat}
+    <Mandat />
+{:else if sidebarState === SIDEBAR_STATES.Devis}
+    <Devis />
+{:else if sidebarState === SIDEBAR_STATES.Reseaux}
+    <Reseaux />
+{/if}
