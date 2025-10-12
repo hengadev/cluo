eae1bbce582f03b638a4e14b4bec0778808366ba
GaryHY <henry.gary@hotmail.com>
2025-10-12 17:55:34 +0200

    feat: add IsPublished field to MediaFile

--- cluo_api/internal/domain/media.go
+++ cluo_api/internal/domain/media.go
@@ -6,16 +6,12 @@ import (
 	"github.com/google/uuid"
 )
 
-// TODO: make the type an enum
-// "image" | "video"
-
-// TODO: add something to precise if that thing is published for the client to see
-
 type MediaFile struct {
-	ID        uuid.UUID
-	CaseID    uuid.UUID `encx:"encrypt"`
-	URL       string    `encx:"encrypt"`
-	Type      string    `encx:"encrypt"`
-	Caption   string    `encx:"encrypt"`
-	CreatedAt time.Time
+	ID          uuid.UUID
+	CaseID      uuid.UUID `encx:"encrypt"`
+	URL         string    `encx:"encrypt"`
+	Type        MediaType `encx:"encrypt"`
+	Caption     string    `encx:"encrypt"`
+	IsPublished bool
+	CreatedAt   time.Time
 }
