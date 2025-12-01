051f8b9a9456ccbcd4833c9b2a0f1b79515de91e
GaryHY <henry.gary@hotmail.com>
2025-12-01 19:37:40 +0100

    feat: add client contact IDs support and DTO structure

--- cluo_api/internal/domain/client/client.go
+++ cluo_api/internal/domain/client/client.go
@@ -8,7 +8,6 @@ import (
 
 type ClientType string
 
-// NOTE: this is just to set the first values, add that to a migration
 const (
 	ClientTypePerson    ClientType = "person"
 	ClientTypeInsurance ClientType = "insurance"
@@ -17,12 +16,9 @@ const (
 )
 
 type Client struct {
-	ID   uuid.UUID
-	Name string `json:"name" encx:"encrypt"`
-	Type string `json:"type" encx:"encrypt"`
-	// Contacts  []string `json:"contacts" encx:"encrypt"` // the list of contact IDs
-	CreatedAt time.Time
+	ID         uuid.UUID
+	Name       string   `json:"name" encx:"encrypt"`
+	Type       string   `json:"type" encx:"encrypt"`
+	ContactIDs []string `json:"contacts" encx:"encrypt"` // the list of contact IDs
+	CreatedAt  time.Time
 }
-
-// client : cabinet avocat
-// contact : someone  that works within the client
