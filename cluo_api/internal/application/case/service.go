71fa13af4b48645cd53925607661d0ea3a0025d3
GaryHY <henry.gary@hotmail.com>
2025-12-07 08:49:07 +0100

    refactor: temporarily comment out unimplemented case service methods
    Comment out service methods that depend on unimplemented repository methods.
    This allows the service to compile while repository methods are implemented incrementally.

--- cluo_api/internal/application/case/service.go
+++ cluo_api/internal/application/case/service.go
@@ -1,45 +1,45 @@
-package application
+package caseService
 
 import (
-	"context"
+	// "context"
 
-	"github.com/hengadev/cluo_api/internal/domain"
+	// "github.com/hengadev/cluo_api/internal/domain"
 	"github.com/hengadev/cluo_api/internal/ports"
+
+	"github.com/hengadev/encx"
 )
 
 type CaseService struct {
-	repo ports.CaseRepository
+	repo   ports.CaseRepository
+	crypto encx.CryptoService
 }
 
-func New(repo ports.CaseRepository) *CaseService {
+func New(repo ports.CaseRepository, crypto encx.CryptoService) *CaseService {
 	return &CaseService{
-		repo: repo,
+		repo:   repo,
+		crypto: crypto,
 	}
 }
 
-func (s *CaseService) CreateCase(ctx context.Context, c *domain.Case) error {
-	return s.repo.Create(ctx, c)
-}
-
-func (s *CaseService) GetCaseByID(ctx context.Context, id string) (*domain.Case, error) {
-	return s.repo.GetByID(ctx, id)
-}
-
-func (s *CaseService) UpdateCase(ctx context.Context, c *domain.Case) error {
-	return s.repo.Update(ctx, c)
-}
-
-func (s *CaseService) DeleteCase(ctx context.Context, id string) error {
-	return s.repo.Delete(ctx, id)
-}
-
-func (s *CaseService) MarkCaseAsReleased(ctx context.Context, id string) error {
-	c, err := s.repo.GetByID(ctx, id)
-	if err != nil {
-		return err
-	}
-
-	c.MarkAsReleased()
-
-	return s.repo.Update(ctx, c)
-}
+// func (s *CaseService) GetCaseByID(ctx context.Context, id string) (*domain.Case, error) {
+// 	return s.repo.GetByID(ctx, id)
+// }
+//
+// func (s *CaseService) UpdateCase(ctx context.Context, c *domain.Case) error {
+// 	return s.repo.Update(ctx, c)
+// }
+//
+// func (s *CaseService) DeleteCase(ctx context.Context, id string) error {
+// 	return s.repo.Delete(ctx, id)
+// }
+//
+// func (s *CaseService) MarkCaseAsReleased(ctx context.Context, id string) error {
+// 	c, err := s.repo.GetByID(ctx, id)
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	c.MarkAsReleased()
+//
+// 	return s.repo.Update(ctx, c)
+// }
