815e45b08e9841168f98a618a13fe832648d570f
GaryHY <henry.gary@hotmail.com>
2025-12-07 09:32:34 +0100

    feat: add authentication middleware to case handler
    Update case handler to use authentication middleware:
    - Fix import path for auth middleware
    - Add authmw field to handler struct
    - Update New() constructor to accept auth middleware

--- cluo_api/internal/interface/case/handler.go
+++ cluo_api/internal/interface/case/handler.go
@@ -3,8 +3,8 @@ package caseHandler
 import (
 	"net/http"
 
+	"github.com/hengadev/cluo_api/internal/common/middleware/auth"
 	"github.com/hengadev/cluo_api/internal/ports"
-	// "github.com/hengadev/cluo_api/core/middleware/auth"
 )
 
 type Handler interface {
@@ -12,12 +12,10 @@ type Handler interface {
 }
 
 type handler struct {
-	svc ports.CaseService
-	// authmw auth.AuthMiddleware
+	svc    ports.CaseService
+	authmw auth.AuthMiddleware
 }
 
-// func New(svc ports.CaseService, authmw auth.AuthMiddleware) Handler {
-func New(svc ports.CaseService) Handler {
-	// return &handler{svc: svc, authmw: authmw}
-	return &handler{svc: svc}
+func New(svc ports.CaseService, authmw auth.AuthMiddleware) Handler {
+	return &handler{svc: svc, authmw: authmw}
 }
