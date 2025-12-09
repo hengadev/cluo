e3192b800a841d85c527fcdacf058b35aeaa1ba7
GaryHY <henry.gary@hotmail.com>
2025-12-09 13:11:43 +0100

    feat: add media type validation method
    Add IsValid() method to MediaType to validate if a media type is one
    of the supported types (image, video, audio). This will be used for
    input validation in media upload handlers.

--- cluo_api/internal/domain/media/media_type.go
+++ cluo_api/internal/domain/media/media_type.go
@@ -8,3 +8,13 @@ const (
 	MediaTypeVideo MediaType = "video"
 	MediaTypeAudio MediaType = "audio"
 )
+
+// IsValid checks if the MediaType is valid
+func (mt MediaType) IsValid() bool {
+	switch mt {
+	case MediaTypeImage, MediaTypeVideo, MediaTypeAudio:
+		return true
+	default:
+		return false
+	}
+}
