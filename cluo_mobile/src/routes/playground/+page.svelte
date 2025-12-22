5eeb19efcf27b352a89b74da08ef6c760e596cee
GaryHY <henry.gary@hotmail.com>
2025-12-22 14:04:51 +0100

    Update main and playground page components

--- cluo_mobile/src/routes/playground/+page.svelte
+++ cluo_mobile/src/routes/playground/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import { Button } from "bits-ui";
+    import { Button } from "$lib/components/ui";
 </script>
 
 <div class="">
-    <Button.Root
+    <Button
         class="rounded-input bg-dark text-background shadow-mini hover:bg-dark/95 inline-flex
 	h-12 items-center justify-center px-[21px] text-[15px]
 	font-semibold active:scale-[0.98] active:transition-all"
     >
         Here is some button that I am trying to make work
-    </Button.Root>
+    </Button>
 </div>
