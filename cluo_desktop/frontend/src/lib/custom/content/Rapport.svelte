3a294d6a5928536d8b657a06f9a2e70ab7e6f54e
GaryHY <henry.gary@hotmail.com>
2025-12-26 13:04:40 +0100

    feat: add grid layout structure to Rapport component

--- cluo_desktop/frontend/src/lib/custom/content/Rapport.svelte
+++ cluo_desktop/frontend/src/lib/custom/content/Rapport.svelte
@@ -1,3 +1,15 @@
-<script lang="ts"></script>
+<script lang="ts">
+    // this part should be so that I can decide on the size of each split
+    // with a maximum width for the one with AI.
+</script>
 
-<div>rapport</div>
+<div class="grid grid-cols-2 h-full">
+    <div class="bg-red-300">rapport the part with the text editor</div>
+    <div class="grid grid-rows-[auto_1fr]">
+        <div
+            class="flex justify-center gap-6 border-b-1 border-dark-50 text-center"
+        >
+            <!-- AI buttons toolbar will go here -->
+        </div>
+    </div>
+</div>
