cf2b316a90e0a451d696a39d76865fcfbce2460d
GaryHY <henry.gary@hotmail.com>
2025-12-26 13:06:22 +0100

    feat: add interactive button snippet with selection state

--- cluo_desktop/frontend/src/lib/custom/content/Rapport.svelte
+++ cluo_desktop/frontend/src/lib/custom/content/Rapport.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    // this part should be so that I can decide on the size of each split
-    // with a maximum width for the one with AI.
     import {
         MessageSquareMore,
         Lightbulb,
         PencilLine,
         AudioLines,
     } from "@lucide/svelte";
+
+    let selected = $state(0);
 
     type AIButton = {
         icon: typeof import("@lucide/svelte").Icon;
@@ -19,7 +19,6 @@
         { icon: PencilLine, title: "Review" },
         { icon: AudioLines, title: "Audio" },
     ];
-    // TODO: make sure that the row 1 just its size and the row takes the remaining space.
 </script>
 
 <div class="grid grid-cols-2 h-full">
@@ -28,7 +27,24 @@
         <div
             class="flex justify-center gap-6 border-b-1 border-dark-50 text-center"
         >
-            <!-- AI buttons toolbar will go here -->
+            {#each aiButtons as item, index}
+                {@render button(index, item)}
+            {/each}
         </div>
     </div>
 </div>
+
+{#snippet button(index: number, item: AIButton)}
+    {@const Icon = item.icon}
+    {@const isSelected = index === selected}
+    <button class="flex gap-2 p-4" onclick={() => (selected = index)}>
+        <Icon
+            size={32}
+            strokeWidth={1.5}
+            class={isSelected ? `text-dark-900` : `text-dark-300`}
+        />
+        <p class={isSelected ? `text-dark-900` : `text-dark-300`}>
+            {item.title}
+        </p>
+    </button>
+{/snippet}
