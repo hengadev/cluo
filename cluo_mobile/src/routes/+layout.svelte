4553394e058b93c0053d51ca3a8c96baab04540b
GaryHY <henry.gary@hotmail.com>
2025-12-26 09:53:23 +0100

    feat: conditionally hide Footer on auth routes
    Exclude Footer from authentication pages (/auth/*) to provide a cleaner login/registration experience. Uses modern $app/state (not deprecated $app/stores) with fine-grained reactivity and Svelte 5 runes for conditional rendering based on pathname

--- cluo_mobile/src/routes/+layout.svelte
+++ cluo_mobile/src/routes/+layout.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
     import "../app.css";
+    import { page } from "$app/state";
 
     import Footer from "./Footer.svelte";
 
     let { children } = $props();
+
+    // Hide footer on auth pages
+    const showFooter = $derived(!page.url.pathname.startsWith("/auth"));
 </script>
 
 <div class="relative px-4">
     {@render children()}
-    <div class="fixed bottom-0 inset-x-0 z-50">
-        <Footer />
-    </div>
+    {#if showFooter}
+        <div class="fixed bottom-0 inset-x-0 z-50">
+            <Footer />
+        </div>
+    {/if}
 </div>
