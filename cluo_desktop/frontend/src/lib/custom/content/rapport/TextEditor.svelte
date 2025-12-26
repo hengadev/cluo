72e0fb85c4d3c429348bc2f5d28149a072f1fb34
GaryHY <henry.gary@hotmail.com>
2025-12-26 21:44:56 +0100

    refactor: extract TextEditor component from Rapport

---
+++ cluo_desktop/frontend/src/lib/custom/content/rapport/TextEditor.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    // Text editor component for rapport/report writing
+    // This will be expanded with rich text editing functionality
+</script>
+
+<div class="flex-1 min-w-0">
+    rapport the part with the text editor
+</div>
