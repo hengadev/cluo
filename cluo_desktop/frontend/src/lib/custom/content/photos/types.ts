f411ab6d65c57e9a8c907fbfd8fd945d4bfc6ea3
GaryHY <henry.gary@hotmail.com>
2026-01-03 16:56:07 +0100

    feat(photos): add SORT_STATES for photo sorting options

--- cluo_desktop/frontend/src/lib/custom/content/photos/types.ts
+++ cluo_desktop/frontend/src/lib/custom/content/photos/types.ts
@@ -6,6 +6,15 @@ export const LAYOUT_STATES = {
 export type LayoutState = typeof LAYOUT_STATES[keyof typeof LAYOUT_STATES];
 export const LAYOUT_STATES_ARRAY = Object.values(LAYOUT_STATES);
 
+export const SORT_STATES = {
+    NewestFirst: "Newest first",
+    OldestFirst: "Oldest first",
+    SelectedFirst: "Selected first",
+    NonSelectedFirst: "NonSelected first",
+} as const;
+
+export type SortState = typeof SORT_STATES[keyof typeof SORT_STATES];
+export const SORT_STATES_ARRAY = Object.values(SORT_STATES);
 
 export type CardType = {
     title: string;
