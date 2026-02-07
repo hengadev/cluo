e881515d9e2f26098314fcf69c69ed8cee49a526
GaryHY <henry.gary@hotmail.com>
2026-02-07 21:36:42 +0100

    feat: add Dockerfile, page server, app route, update config

--- cluo_web/src/routes/+page.svelte
+++ cluo_web/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import type { ActionData } from "./$types";
+    // let { data, form } = $props();
+    let { form }: ActionData = $props();
+
+    let id: string = $state("");
+    let password: string = $state("");
+</script>
+
+<form method="POST">
+    <div class="field">
+        <label for="id">Numero de dossier</label>
+        <input type="text" name="id" id="id" bind:value={id} />
+    </div>
+    <div class="field">
+        <label for="id">Numero de dossier</label>
+        <input type="text" name="id" id="id" bind:value={password} />
+    </div>
+</form>
+{#if form?.incorrect}
+    <p>here I put something if not well done</p>
+{/if}
+
+<style>
+</style>
